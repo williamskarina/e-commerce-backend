c6862fdcf5174175c1064606a5abe9f2e0f79f1c
Karina Williams <coleman_karina@yahoo.com>
2021-12-02 13:09:48 -0800

    product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,60 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: "tags",
+      },
+    ]
+  })
   // be sure to include its associated Category and Tag data
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attribtues: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: "tags",
+      },
+    ]
+  })
+  .then((dbProductData) => {
+    if (!dbProductData) {
+      res
+        .status(404)
+        .json({ message: "There was no product found associated with this id"});
+        return;
+    }
+    res.json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // be sure to include its associated Category and Tag data
 });
 
